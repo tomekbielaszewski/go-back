634117e1749274a5129eb5a8a1328c28ce34b2b9
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2021-09-09 17:42:40 +0200

    basic commands to interact with glacier archive

---
+++ cmd/downloadCmd.go
@@ -0,0 +1,18 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var shortId string
+
+var downloadCmd = &cobra.Command{
+	Use:   "",
+	Short: "",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+func init() {
+	downloadCmd.Flags().StringVarP(&shortId, "short-id", "id", "", "ID of backed up resource. List your available resources with \"list\" command")
+	rootCmd.AddCommand(downloadCmd)
+}
