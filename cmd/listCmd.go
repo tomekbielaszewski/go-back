cda5e2b52a8701e519687e7d6f5e247e9cd05711
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2021-09-15 22:57:12 +0200

    skeleton of list command

--- cmd/listCmd.go
+++ cmd/listCmd.go
@@ -1,15 +1,49 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+	"time"
+)
+
+func init() {
+	listCmd.Flags().BoolVarP(&forceUpdate, "force-update", "f", false,
+		"Forces archive list update from live Glacier.")
+	rootCmd.AddCommand(listCmd)
+}
 
 var listCmd = &cobra.Command{
-	Use:   "",
-	Short: "",
+	Use:   "list",
+	Short: "list backed up archives",
 	Run: func(cmd *cobra.Command, args []string) {
-
+		if isUpdateForced() {
+			updateArchives()
+		}
+		printArchives()
 	},
 }
 
-func init() {
-	rootCmd.AddCommand(listCmd)
+func printArchives() {
+
 }
+
+func updateArchives() {
+
+}
+
+func isUpdateForced() bool {
+	return forceUpdate || isArchiveStale()
+}
+
+func isArchiveStale() bool {
+	return time.Now().After(lastUpdate().Add(stalePeriod()))
+}
+
+func stalePeriod() time.Duration {
+	return time.Hour * 24 //todo this should ne taken from state file
+}
+
+func lastUpdate() time.Time {
+	return time.Now() //todo it should be taken from state file
+}
+
+var forceUpdate bool
