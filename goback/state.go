5196586d06e6785f68b246a0523c05b7922745ee
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2021-09-15 23:58:07 +0200

    sample models

---
+++ goback/state.go
@@ -0,0 +1,19 @@
+package goback
+
+import "time"
+
+type archive struct {
+	id   string `json:"id"`
+	name string `json:"name"`
+	md5  string `json:"md5"`
+	size int32  `json:"size"`
+}
+
+func (a *archive) ToString() string {
+
+}
+
+type state struct {
+	updated  time.Time `json:"updated"`
+	archives []archive `json:"archives"`
+}
