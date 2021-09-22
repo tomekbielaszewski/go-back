11792d0577ab6996eba6e4fe193d1ea1bba0b173
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2021-09-22 23:46:53 +0200

    sample bucket listing code

--- cmd/listCmd.go
+++ cmd/listCmd.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"context"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/spf13/cobra"
+	"log"
 	"time"
 )
 
@@ -14,6 +19,26 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "list backed up archives",
 	Run: func(cmd *cobra.Command, args []string) {
+
+		defaultConfig, err := config.LoadDefaultConfig(context.TODO())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		s3Client := s3.NewFromConfig(defaultConfig)
+
+		objects, err := s3Client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+			Bucket: aws.String("some-bucket"),
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("Printing stuff:")
+		for _, object := range objects.Contents {
+			log.Printf("key=%s size=%d", aws.ToString(object.Key), object.Size)
+		}
+
 		if isUpdateForced() {
 			updateArchives()
 		}
