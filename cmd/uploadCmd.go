a735afdd13dd5593ce15d8b1a5fa10409acde1d8
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2021-09-22 23:46:20 +0200

    sample bucket listing code

--- cmd/uploadCmd.go
+++ cmd/uploadCmd.go
@@ -1,8 +1,13 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/spf13/cobra"
+	"log"
 )
 
 type UploadCmdArgs struct {
@@ -23,5 +28,24 @@ var uploadCmd = &cobra.Command{
 	Short: "uploads the archive",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(uploadCmdArgs)
+
+		defaultConfig, err := config.LoadDefaultConfig(context.TODO())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		s3Client := s3.NewFromConfig(defaultConfig)
+
+		objects, err := s3Client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+			Bucket: aws.String("some-bucket"),
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("Printing stuff:")
+		for _, object := range objects.Contents {
+			log.Printf("key=%s size=%d", aws.ToString(object.Key), object.Size)
+		}
 	},
 }
